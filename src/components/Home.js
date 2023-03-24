f277ecce59a4fe0bb7ff049818fe2611e7c789fe
Jason Park <jjpark987@gmail.com>
2023-03-23 19:19:49 -0700

    Start README.md

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,11 @@ import React from "react";
 function Home() {
     return (
         <div id="home">
-            <img src="public/cooking_image.jpeg" alt="food_image" />
             <h1>Welcome to Recipe Journal!</h1>
-            <h3>Let's get cooking!</h3>
-            <img src="/public/cooking_image.jpeg" alt="cooking_image" />
+            <img width="25%" src="/food_image.jpeg" alt="food_image" />
+            <h3>Click on "Recipes" to see a list of recipes.</h3>
+            <h3>Click on "Add Recipe" to add a new recipe to the list.</h3>
+            <h3>Let's start cooking!</h3>
         </div>
     );
 }
